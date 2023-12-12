13c64d50ed1244275ae53228a5aabfa8aa8d83ce
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-12 10:18:42 +0200

    update

--- src/index.js
+++ src/index.js
@@ -122,16 +122,16 @@ function renderMarkup(hits) {
     <img src="${webformatURL}" alt="${tags}" width = "300" loading="lazy" />
     <div class="info">
       <p class="info-item">
-        <b>Likes'${likes}'</b>
+        <b>Likes ${likes}</b>
       </p>
       <p class="info-item">
-        <b>Views'${views}'</b>
+        <b>Views ${views}</b>
       </p>
       <p class="info-item">
-        <b>Comments'${comments}'</b>
+        <b>Comments ${comments}</b>
       </p>
       <p class="info-item">
-        <b>Downloads'${downloads}'</b>
+        <b>Downloads ${downloads}</b>
       </p>
     </div>
   </div>`
