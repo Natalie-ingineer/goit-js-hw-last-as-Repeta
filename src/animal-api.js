32196f087089a1e08d1f9f3ffeb90469d17e38b4
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-12 15:41:18 +0200

    update

--- src/animal-api.js
+++ src/animal-api.js
@@ -1,116 +1,20 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '41079066-0341c17d8bd684537c8a66e3e';
+// const BASE_URL = 'https://pixabay.com/api/';
+// const API_KEY = '41079066-0341c17d8bd684537c8a66e3e';
 
-export default class NewsApiService {
-  constructor() {
-    this.searchAnimal = '';
-    this.page = 1;
-    this.per_page = 40;
-  }
+// const queryParams = {
+//   key: API_KEY,
+//   q: `${this.searchAnimal}`,
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: true,
+//   page: `${this.page}`,
+//   per_page: `${this.per_page}`,
+// };
 
-  fetchPegs() {
-    const option = {
-      key: API_KEY,
-      safesearch: true,
-      page: `${this.page}`,
-      per_page: `${this.per_page}`,
-    };
-    return axios
-      .get(BASE_URL, { params: option })
-      .then(response => {
-        // console.log(response.data.hits);
-        this.incrementPage();
-        return response.data.totalHits;
-      })
-      .catch(error => {
-        console.log(error);
-        console.log(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      });
-  }
-
-  fetchHits() {
-    const queryParams = {
-      key: API_KEY,
-      q: `${this.searchAnimal}`,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      page: `${this.page}`,
-      per_page: `${this.per_page}`,
-    };
-    return axios
-      .get(BASE_URL, { params: queryParams })
-      .then(response => {
-        // console.log(response.data.hits);
-        this.incrementPage();
-        return response.data.hits;
-      })
-      .catch(error => {
-        console.log(error);
-        console.log(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      });
-  }
-
-  incrementPage() {
-    this.page += 1;
-  }
-
-  resetPage() {
-    this.page = 1;
-  }
-
-  get animal() {
-    return this.searchAnimal;
-  }
-
-  set animal(newSearchAnimal) {
-    this.searchAnimal = newSearchAnimal;
-  }
-}
-
-// import SlimSelect from 'slim-select';
-
-// import '/node_modules/slim-select/dist/slimselect.css';
-
-// import '/node_modules/slim-select/dist/slimselect.min.js';
-
-// axios.defaults.headers.common['x-api-key'] =
-//   'live_QnYFBW48Rw6I7bGfsFA1QTbYdWtKQKT86j8h8KpF4TCTSz8rp4W4DFTADxItBVig';
-
-// const BASE_URL = 'https://api.thecatapi.com/v1/';
-// const END_POINTS = 'breeds';
-
-// const KEY =
-//   'live_QnYFBW48Rw6I7bGfsFA1QTbYdWtKQKT86j8h8KpF4TCTSz8rp4W4DFTADxItBVig';
-
-// export function fetchBreeds() {
-//   return fetch(`${BASE_URL}${END_POINTS}?api_key=${KEY}`).then(responce => {
-//     if (!responce.ok) {
-//       throw new Error(responce.statusText);
-//     }
-
-//     new SlimSelect({
-//       select: '#selectElement',
-//     });
-//     return responce.json();
-//   });
+// async function foo() {
+//   const data = await axios(BASE_URL, queryParams);
+//   console.log(data);
 // }
-
-// export function fetchCatByBreed(breedId) {
-//   const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
-
-//   return fetch(`${BASE_URL}?api_key=${KEY}&breed_ids=${breedId}`).then(
-//     responce => {
-//       if (!responce.ok) {
-//         throw new Error(responce.statusText);
-//       }
-//       return responce.json();
-//     }
-//   );
-// }
+// foo();
--- src/index.js
+++ src/index.js
@@ -1,173 +1,166 @@
-import NewsApiService from './animal-api';
+// import NewsApiService from './animal-api';
 
-// import SlimSelect from 'slim-select';
+// const searchForm = document.querySelector('.search-form');
+// const btnSubmit = document.querySelector('button');
+// const loadMore = document.querySelector('.load-more');
+// const divGallery = document.querySelector('.gallery');
 
-// import '/node_modules/slim-select/dist/slimselect.css';
+// const newsApiService = new NewsApiService();
 
-// import '/src/loader.css';
+// searchForm.addEventListener('submit', handlerSearch);
+// loadMore.addEventListener('click', onLoadMore);
+// let totalHits = 500;
+// let hits = 0;
 
-// import Notiflix from 'notiflix';
+// loadMore.style.display = 'none';
+// divGallery.style.display = 'flex';
+// divGallery.style.flexWrap = 'wrap';
 
-// import Notiflix from 'notiflix/dist/notiflix-aio-3.2.6.min.js';
+// function handlerSearch(e) {
+//   e.preventDefault();
 
-// Notiflix.Notify.init({
-//   width: '280px',
-//   position: 'right-top', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
-//   distance: '10px',
-//   opacity: 1,
-//   borderRadius: '5px',
-//   rtl: false,
-//   timeout: 3000,
-//   messageMaxLength: 110,
-//   backOverlay: false,
-//   backOverlayColor: 'rgba(0,0,0,0.5)',
-//   plainText: true,
-//   showOnlyTheLastOne: false,
-//   clickToClose: false,
-//   pauseOnHover: true,
+//   newsApiService.animal = e.currentTarget.searchQuery.value;
 
-//   ID: 'NotiflixNotify',
-//   className: 'notiflix-notify',
-//   zindex: 4001,
-//   fontFamily: 'Quicksand',
-//   fontSize: '13px',
-//   cssAnimation: true,
-//   cssAnimationDuration: 400,
-//   cssAnimationStyle: 'fade', // 'fade' - 'zoom' - 'from-right' - 'from-top' - 'from-bottom' - 'from-left'
-//   closeButton: false,
-//   useIcon: true,
-//   useFontAwesome: false,
-//   fontAwesomeIconStyle: 'basic', // 'basic' - 'shadow'
-//   fontAwesomeIconSize: '34px',
+//   if (newsApiService.animal === '') {
+//     return alert('Oops!');
+//   }
 
-//   failure: {
-//     background: '#ff5549',
-//     textColor: '#fff',
-//     childClassName: 'notiflix-notify-failure',
-//     notiflixIconColor: 'rgba(0,0,0,0.2)',
-//     fontAwesomeClassName: 'fas fa-times-circle',
-//     fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-//     backOverlayColor: 'rgba(255,85,73,0.2)',
-//   },
-// });
+//   newsApiService.resetPage();
+//   newsApiService.fetchHits().then(hits => {
+//     clearDivContainer();
+//     createMarkupAnimals(hits);
 
-const searchForm = document.querySelector('.search-form');
-const btnSubmit = document.querySelector('button');
-const loadMore = document.querySelector('.load-more');
-const divGallery = document.querySelector('.gallery');
-
-const newsApiService = new NewsApiService();
-
-searchForm.addEventListener('submit', handlerSearch);
-loadMore.addEventListener('click', onLoadMore);
-let totalHits = 500;
-let hits = 0;
-
-loadMore.style.display = 'none';
-divGallery.style.display = 'flex';
-divGallery.style.flexWrap = 'wrap';
-
-// newsApiService.fetchHits().then(totalHits => console.log(totalHits));
-
-function handlerSearch(e) {
-  e.preventDefault();
-
-  newsApiService.animal = e.currentTarget.searchQuery.value;
-
-  if (newsApiService.animal === '') {
-    return alert('Oops!');
-  }
-
-  newsApiService.resetPage();
-  newsApiService.fetchHits().then(hits => {
-    clearDivContainer();
-    createMarkupAnimals(hits);
-
-    if (hits.length < totalHits) {
-      onLoaderVisible();
-    } else if (hits.length === totalHits) {
-      onLoaderHidden();
-    }
-  });
-
-  // newsApiService.fetchPegs().then(totalHits => {
-  //   console.log(totalHits);
-  //   if (hits.length < totalHits) {
-  //     onLoaderVisible();
-  //   }
-  //   //   onLoaderHidden();
-  //   //   return alert("We're sorry, but you've reached the end of search results!");
-  // });
-}
-// btnSubmit.addEventListener('click', handlerSubmit);
-
-function onLoadMore() {
-  newsApiService.fetchHits().then(createMarkupAnimals);
-}
-
-function renderMarkup(hits) {
-  // const animalHits = hits[0];
-
-  return hits
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `<div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" width = "300" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes ${likes}</b>
-      </p>
-      <p class="info-item">
-        <b>Views ${views}</b>
-      </p>
-      <p class="info-item">
-        <b>Comments ${comments}</b>
-      </p>
-      <p class="info-item">
-        <b>Downloads ${downloads}</b>
-      </p>
-    </div>
-  </div>`
-    )
-    .join('');
-}
-
-function createMarkupAnimals(hits) {
-  divGallery.insertAdjacentHTML('beforeend', renderMarkup(hits));
-}
-
-function clearDivContainer() {
-  divGallery.innerHTML = '';
-}
-
-// ----------------------------------------------------------------------------------
-
-// function createMarkupCat(data) {
-//   const cat = data[0].breeds[0];
-
-//   const { name, description, temperament } = cat;
-
-//   return `
-//   <img src="${data[0].url}" alt="${name}" width="450">
-//   <h2>${name}</h2>
-//   <p>${description}</p>
-//   <p>Temperament: ${temperament}</p>
-//   `;
+//     if (hits.length < totalHits) {
+//       onLoaderVisible();
+//     } else if (hits.length === totalHits) {
+//       onLoaderHidden();
+//     }
+//   });
 // }
 
-function onLoaderVisible() {
-  loadMore.style.display = 'block';
-  // loadMore.textContent = '';
-}
+// function onLoadMore() {
+//   newsApiService.fetchHits().then(createMarkupAnimals);
+// }
 
-function onLoaderHidden() {
-  loadMore.style.display = 'none';
-  // div.style.display = 'block';
-}
+// function renderMarkup(hits) {
+//   // const animalHits = hits[0];
+
+//   return hits
+//     .map(
+//       ({
+//         webformatURL,
+//         largeImageURL,
+//         tags,
+//         likes,
+//         views,
+//         comments,
+//         downloads,
+//       }) => `<div class="photo-card">
+//     <img src="${webformatURL}" alt="${tags}" width = "300" loading="lazy" />
+//     <div class="info">
+//       <p class="info-item">
+//         <b>Likes ${likes}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Views ${views}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Comments ${comments}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Downloads ${downloads}</b>
+//       </p>
+//     </div>
+//   </div>`
+//     )
+//     .join('');
+// }
+
+// function createMarkupAnimals(hits) {
+//   divGallery.insertAdjacentHTML('beforeend', renderMarkup(hits));
+// }
+
+// function clearDivContainer() {
+//   divGallery.innerHTML = '';
+// }
+
+// function onLoaderVisible() {
+//   loadMore.style.display = 'block';
+// }
+
+// function onLoaderHidden() {
+//   loadMore.style.display = 'none';
+// }
+
+// // ----------------------------------------------------------------------------------
+
+import axios from 'axios';
+
+const API_KEY = '41079066-0341c17d8bd684537c8a66e3e';
+const BASE_URL =
+  'https://pixabay.com/api/?key=41079066-0341c17d8bd684537c8a66e3e';
+
+const queryParams = {
+  key: API_KEY,
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: 'page',
+  per_page: 'per_page',
+};
+
+// async function foo() {
+//   const data = await axios(BASE_URL);
+//   console.log(data);
+// }
+// foo();
+
+// те саме що і
+
+// axios(BASE_URL).then(response => console.log(response));
+
+// 1
+// async function foo() {
+//   try {
+//     const data = await axios(BASE_URL);
+//     console.log(data.config);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+//   console.log('end');
+// }
+
+// foo();
+
+// 2
+// async function foo() {
+//   const response = await axios(BASE_URL);
+//   return response.data;
+// }
+
+// foo()
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error.message));
+
+// 3
+// async function foo() {
+//   const response = await axios(BASE_URL);
+//   return response.data;
+// }
+
+// async function result() {
+//   try {
+//     const data = await foo();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// result();
+
+// async function getRequst() {
+//   const value = await axios(BASE_URL)
+
+// }
